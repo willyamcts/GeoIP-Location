743dc4197e3684327b7b71ce5b5b3e8ac4dbcb1c
Willyam Castro <willyam.android@gmail.com>
2026-04-21 13:05:07 -0300

    created a simple log file; cleaner output; fixed execution when there are no changes in the database.

--- geoip-create_db.sh
+++ geoip-create_db.sh
@@ -1,8 +1,9 @@
 LINK_NRO_STATS="https://ftp.ripe.net/pub/stats/ripencc/nro-stats/latest/nro-delegated-stats"
 LINK_NRO_MD5="https://ftp.ripe.net/pub/stats/ripencc/nro-stats/latest/nro-delegated-stats.md5"
-DB_DIR=/usr/local/geodb/db
-TMP_DB=/usr/src/geoip
+DB_DIR=/usr/local/geoip/db
+TMP_DB=/var/tmp/geoip
 TMP_FILE=$TMP_DB/nro-delegated-stats
+LOG_FILE=/var/log/geoipdb.log
 declare -A masks
 
 convertQttAddrToPrefix() {
@@ -15,18 +16,20 @@ convertQttAddrToPrefix() {
 clear
 if [[ ! -d $DB_DIR || ! -d $TMP_DB ]]; then
   mkdir -p $DB_DIR/{ipv4,ipv6} $TMP_DB
-  echo "Create dir $DB_DIR $TMP_DB"
+  echo "* Creating dir $DB_DIR $TMP_DB"
 else
-  echo "Directories $DB_DIR $TMP_DB already"
+  echo "* Directories $DB_DIR $TMP_DB already"
 fi
 
 md5_remote=$(wget -q -O- $LINK_NRO_MD5 |awk '{print $1}')
-md5_local=$(md5sum $TMP_FILE |awk '{print $1}') #&2>/dev/null)
+[[ -n $(ls $TMP_FILE-* 2>/dev/null) ]] && \
+  md5_local=$(md5sum $(ls -t $TMP_FILE-* |head -n1) |awk '{print $1}') #&2>/dev/null)
 
 if [[ $md5_remote != $md5_local ]]; then
   # download db
-  echo "Downloading $LINK_NRO_STATS"; echo
-  wget --no-check-certificate -O $TMP_FILE $LINK_NRO_STATS || exit 1
+  echo " * Downloading $LINK_NRO_STATS" | tee -a $LOG_FILE
+  wget -nv --show-progress --no-check-certificate -O $TMP_FILE $LINK_NRO_STATS || exit 1
+  echo
 
   # set new name with creation time
   stat_file=$(stat -c %y $TMP_FILE) # -c %w
@@ -35,33 +38,38 @@ if [[ $md5_remote != $md5_local ]]; then
   utc_file=$(echo $stat_file |awk '{print $3}')
   new_name="${TMP_FILE}-${date_file}-${hour_file}-UTC${utc_file}"
   cp --preserve $TMP_FILE $new_name
-  echo "DB file creation date: $date_file $hour_file UTC $utc_file"
-else
-  echo "Your database already updated"
-fi
+  echo "* DB file creation date: $date_file $hour_file UTC $utc_file" | tee -a $LOG_FILE
 
 
-# clear file with IPv4 and IPv6 only
-sed -Ei '/ipv4|ipv6/!d; /summary/d' $TMP_FILE
+  # clear file with IPv4 and IPv6 only
+  sed -Ei '/ipv4|ipv6/!d; /summary/d' $TMP_FILE
 
-countries=$(awk -F"|" '{print $2}' $TMP_FILE |sort -u)
+  countries=$(awk -F"|" '{print $2}' $TMP_FILE |sort -u)
 
-# set array map address available to network prefix
-convertQttAddrToPrefix
+  # set array map address available to network prefix
+  convertQttAddrToPrefix
 
-# replace quantitty IP to mask
-echo "Converting information IPv4 to network prefix"
-for qtt_address in ${!masks[@]}; do
-  printf " - prefix: ${masks[$qtt_address]} \t==\t qtt_addr: ${qtt_address} \t|\t changes: $(grep -c "|${qtt_address}|" $TMP_FILE)\n"
-  sed -i "s#|${qtt_address}|#|${masks[$qtt_address]}|#g" $TMP_FILE
-done
-
-rm ${DB_DIR}/{ipv4,ipv6}/*.zone
-# create database directory
-echo "Creating DB for: "
-for code in $countries; do
-  printf " $code"
-  for version in ipv4 ipv6; do
-    grep $code $TMP_FILE | grep $version | awk -F"|" '{print $4"/"$5}' >> "${DB_DIR}/${version}/${code,,}.zone"
+  # replace quantitty IP to mask
+  echo " * Converting information IPv4 to network prefix"
+  for qtt_address in ${!masks[@]}; do
+    printf "  - prefix: ${masks[$qtt_address]}\t==\tqtt_prefix = $(grep -c "|${qtt_address}|" $TMP_FILE)\n" | tee -a $LOG_FILE
+    sed -i "s#|${qtt_address}|#|${masks[$qtt_address]}|#g" $TMP_FILE
   done
-done
+
+  rm ${DB_DIR}/{ipv4,ipv6}/*.zone
+  # create database directory
+  echo; echo "* Creating DB in $DB_DIR for: "
+  for code in $countries; do
+    printf " $code"
+    for version in ipv4 ipv6; do
+      grep $code $TMP_FILE | grep $version | awk -F"|" '{print $4"/"$5}' >> "${DB_DIR}/${version}/${code,,}.zone"
+    done
+  done
+  echo
+
+else
+  echo "* Your database already updated - ${md5_local}" && \
+    echo "
+$(date +%d-%m-%Y" "%H:%M) Your database already updated - ${md5_local}
+" >> $LOG_FILE
+fi
